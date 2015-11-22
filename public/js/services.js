530ae00d5d41d085582178c565133578bad00d5a
nether_nova <martin.ringsquandl@googlemail.com>
2015-11-22 18:31:17 +0100

    services relative

--- public/js/services.js
+++ public/js/services.js
@@ -5,14 +5,14 @@ var imageServices = angular.module('imageServices', ['ngResource']);
 
 sensorServices.factory('Sensors', ['$resource',
     function($resource) {
-        return $resource("http://localhost:8000/sensors", {}, {
+        return $resource("/sensors", {}, {
             get : {method: 'GET', cache: false, isArray : true}
         });
     }]);
 
 imageServices.factory('Images', ['$resource',
     function($resource) {
-        return $resource("http://localhost:8000/images", {}, {
+        return $resource("/images", {}, {
             get : {method: 'GET', cache: false, isArray : false}
         });
     }]);
